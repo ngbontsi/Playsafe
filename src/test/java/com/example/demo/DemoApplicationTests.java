2646b1d0c03e3aa9ca798b3094c42dd01b50d5e4
ngbontsi <ngbontsi@gmail.com>
2020-04-16 12:27:57 +0200

    adding tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,10 +4,45 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DemoApplicationTests extends DemoBuilder{
 
 	@Test
 	void contextLoads() {
+
+	 String ktocUri = "/conversions/ktoc";
+	 String ktomUri = "/conversions/ktom";
+	 String ctokUri = "/conversions/ctok";
+	 String mtokUri = "/conversions/mtok";
+
+	   MvcResult ktocResult = mvc.perform(MockMvcRequestBuilders.get(ktocUri)
+	      .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	   MvcResult ktomResult = mvc.perform(MockMvcRequestBuilders.get(ktomUri)
+	      .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	   MvcResult ctokResult = mvc.perform(MockMvcRequestBuilders.get(ctokUri)
+	      .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	   MvcResult mtokResult = mvc.perform(MockMvcRequestBuilders.get(mtokUri)
+	      .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+	   int ktocStatus = ktocResult.getResponse().getStatus();
+	   assertEquals(200, ktocStatus);
+	   System.out.println(ktocResult.getResponse().getContentAsString());
+	
+	   int ktomsStatus = ktomResult.getResponse().getStatus();
+	   assertEquals(200, ktomsStatus);
+	   System.out.println(ktomResult.getResponse().getContentAsString());
+
+	   int ctokStatus = ctokResult.getResponse().getStatus();
+	   assertEquals(200, ctokStatus);
+	   System.out.println(ctokResult.getResponse().getContentAsString());
+
+	   int mtokStatus = mtokResult.getResponse().getStatus();
+	   assertEquals(200, mtokStatus);
+	   System.out.println(mtokResult.getResponse().getContentAsString());
+	
+	
+
+
+
 	}
 
 }
