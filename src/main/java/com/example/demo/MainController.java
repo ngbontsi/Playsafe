1934b632a114dc13061b4d37ea59f81aa5dd2349
ngbontsi <ngbontsi@gmail.com>
2020-04-16 11:48:17 +0200

    edit main controller

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -8,33 +8,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloWorldController {
+public class MainController {
 
-  private static final String template = "Hello, %s!";
+  private static final String template = "You have called method , %s!";
   private final AtomicLong counter = new AtomicLong();
 
   @GetMapping("/conversions/ktoc")
   @ResponseBody
-  public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+  public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="ktoc") String name) {
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
   }
 
 
 @GetMapping("/conversions/ctok")
   @ResponseBody
-  public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+  public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="ctok") String name) {
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
   }
 
 @GetMapping("/conversions/mtok")
   @ResponseBody
-  public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+  public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="mtok") String name) {
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
   }
 
 @GetMapping("/conversions/ktom")
   @ResponseBody
-  public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+  public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="ktom") String name) {
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
   }
 
